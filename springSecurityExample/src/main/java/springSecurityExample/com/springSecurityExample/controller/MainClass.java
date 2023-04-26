8b57df8edc00109c589efcb30773a038102addc8
yogeshseroke <99466928+yogeshseroke@users.noreply.github.com>
2023-04-26 11:58:14 +0530

    Add files via upload

---
+++ springSecurityExample/src/main/java/springSecurityExample/com/springSecurityExample/controller/MainClass.java
@@ -0,0 +1,10 @@
+package springSecurityExample.com.springSecurityExample.controller;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
